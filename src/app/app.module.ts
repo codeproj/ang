ca7f92aef7b94aefcbb5dc751fef29ffbd67f8c8
nadoman <nado261@gmail.com>
2022-11-24 12:27:16 +0200

    add global files as well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NadavcComponent } from './nadavc/nadavc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NadavcComponent
   ],
   imports: [
     BrowserModule,
